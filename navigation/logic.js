7081f0be27fd738a41c0bfae8fd37d36e5318d60
shashank21singh <shashank212020@gmail.com>
2024-07-15 20:48:22 +0530

    Add files via upload

---
+++ navigation/logic.js
@@ -0,0 +1,34 @@
+const btn =document.querySelector("button");
+const home =document.querySelector("i");
+const search =document.querySelector(".search input");
+const gift =document.querySelector(".offer i");
+const task =document.querySelector(".tasks h3");
+const events =document.querySelector(".event h3");
+const career =document.querySelector(".Career h3");
+const contact =document.querySelector(".contact h3");
+const button =document.querySelector(".dropbtn");
+
+home.onmouseover =() =>{
+    document.body.style.backgroundColor='orange';
+} 
+gift.onmouseover =() =>{
+    document.body.style.backgroundColor='red';
+    
+    } 
+search.onmouseover =() =>{
+    document.body.style.backgroundColor='grey';
+    } 
+    task.onmouseover =() =>{
+        document.body.style.backgroundColor='yellow';
+    } 
+    events.onmouseover =() =>{
+    document.body.style.backgroundColor='brown';
+    } 
+    career.onmouseover =() =>{
+        document.body.style.backgroundColor='pink';
+        } 
+    contact.onmouseover =() =>{
+        document.body.style.backgroundColor='silver';
+        } 
+        
+            
